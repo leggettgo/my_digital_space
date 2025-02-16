5a7f423ec8700e2f35304a1b017857869af091e4
Alex Leggett <alexleggett123@gmail.com>
2025-02-16 21:54:56 +0100

    Add footer

---
+++ footer.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const footer = `
+        <footer class="footer">
+            Built by Alex in 2025 ☕
+        </footer>
+    `;
+
+    document.body.insertAdjacentHTML("beforeend", footer);
+});
