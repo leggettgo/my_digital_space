08ec7a7ed768c50e584bba1d9c023fb384dc7be6
Alex Leggett <alexleggett123@gmail.com>
2026-01-01 11:40:05 +0100

    Remove deprecated JavaScript files

--- footer.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const footer = `
-        <footer class="footer">
-            Built by Alex in 2025 ☕
-        </footer>
-    `;
-
-    document.body.insertAdjacentHTML("beforeend", footer);
-});
